1358f1b630fa93a58e3caaec997f9dc690eb1d50
monarchSatan <133543243+SatanPemula@users.noreply.github.com>
2024-07-10 23:10:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
+            console.log(data); // Untuk debugging
             statusContainer.innerHTML = `
-                <h2>Status: ${data.status}</h2>
-                <p>Players online: ${data.players_online}</p>
+                <h2>Status: Online</h2>
+                <p>Players online: ${data.online_user}</p>
             `;
         })
         .catch(error => {
+            console.error('Fetch error:', error);
             statusContainer.innerHTML = `<p>Error fetching data: ${error}</p>`;
         });
 });
